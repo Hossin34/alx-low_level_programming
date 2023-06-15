b3f77c2933ee5a4ee0521d29b73fd06329714bb4
Hossin34 <hossinsohail.com>
2023-06-15 07:59:55 -0700

    7th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Main function of the program
+*
+*
+*Return: return 0 on success
+*/
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
