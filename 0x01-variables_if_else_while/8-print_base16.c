206f905e8f0c881e91b2400f136b6707ca61516d
Hossin34 <hossinsohail.com>
2023-06-16 14:14:26 -0700

    new one

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit;
+       	int i;
+       	char letter;
+
+	digit = '0';
+	letter = 'a';
+	for (i = 0; i < 10; i++)
+	{
+		putchar(digit);
+		digit++;
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
