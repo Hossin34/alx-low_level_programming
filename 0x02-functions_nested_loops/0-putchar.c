1681efdce323ee08209120b99dac5de8f0aad8bf
Hossin34 <hossinsohail.com>
2023-06-20 03:14:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+*main - Main function of the program
+*
+*
+*Return: return 0 on success
+*/
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
