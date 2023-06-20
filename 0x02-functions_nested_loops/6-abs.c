3ab08c83c63230995faf797ab6eb43bd2e3fc82a
Hossin34 <hossinsohail.com>
2023-06-20 15:47:33 -0700

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of an number
+ * @n: number to convert to abs
+ * Return: Void.
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
