a4b5f6fc02eb9e8e1fab379a6b7b264ffd4745ce
Hossin34 <hossinsohail.com>
2023-06-21 12:34:56 -0700

    new project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - debug positive_or_negative function
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
