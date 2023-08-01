4e2624e5f7695d8edfb1bfb5031805dafcd1adcc
Hossin34 <hossinsohail.com>
2023-08-01 10:46:57 -0700

    dunno what is wrong with ex0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
  * @h: pointer to head of list
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
@@ -17,4 +18,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (count);
 }
-
